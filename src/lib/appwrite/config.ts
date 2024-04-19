9e0705d145bfa71c33c7b096920ded70bd73edf8
Adarsh Amit <adarsh.amit1001@gmail.com>
2024-04-20 00:16:05 +0530

    Added toast library for nice alerts

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -2,6 +2,11 @@ import { Client, Account, Databases, Storage, Avatars } from "appwrite";
 export const appwriteConfig = {
   projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
   url: import.meta.env.VITE_APPWRITE_URL,
+  databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+  storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+  userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
+  pitchCollectionId: import.meta.env.VITE_APPWRITE_PITCH_COLLECTION_ID,
+  savedCollectionId: import.meta.env.VITE_APPWRITE_SAVED_COLLECTION_ID,
 };
 export const client = new Client();
 client.setProject(appwriteConfig.projectId);
